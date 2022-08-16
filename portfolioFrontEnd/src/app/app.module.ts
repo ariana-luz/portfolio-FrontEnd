74ff3ccec5c8729aebf51fca5c8d78e5fcb61274
Ariana <arianaosunavargas92@gmail.com>
2022-08-15 21:12:32 -0300

    Separación del body en componentes

--- portfolioFrontEnd/src/app/app.module.ts
+++ portfolioFrontEnd/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaYEducacionComponent } from './componentes/experiencia-y-educacion/experiencia-y-educacion.component';
+import { LogrosComponent } from './componentes/logros/logros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercaDeComponent,
+    ExperienciaYEducacionComponent,
+    LogrosComponent
   ],
   imports: [
     BrowserModule,
---
+++ portfolioFrontEnd/src/app/componentes/logros/logros.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logros',
+  templateUrl: './logros.component.html',
+  styleUrls: ['./logros.component.less']
+})
+export class LogrosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
